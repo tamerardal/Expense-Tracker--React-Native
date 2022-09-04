8b95c8aa6e45786df1f947ba4087a3327e70229b
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-09-04 17:27:00 +0300

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Route from './src/App';
+import Route from './src/Router';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Route);
