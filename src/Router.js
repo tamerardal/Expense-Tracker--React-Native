021e090322646021ba5f8b4e9e2811b9cb71d147
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-09-05 12:49:47 +0300

    stil

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
---
+++ src/components/MyExpensesCard/MyExpensesCard.styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  incomeCon: {
+    flex: 1,
+    backgroundColor: '#32cd32',
+    padding: 5,
+    margin: 5,
+    borderRadius: 5,
+  },
+  expenseCon: {
+    backgroundColor: '#32cd32',
+    flex: 1,
+    padding: 5,
+    margin: 5,
+    borderRadius: 5,
+  },
+  title: {fontSize: 22, color: 'white'},
+  price: {},
+});
