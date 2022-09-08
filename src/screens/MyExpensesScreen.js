8c1fdbf5e28873e6fd4ded42771806cba170f788
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-09-08 15:16:13 +0300

    gereksiz kodlar silindi, piechart eklendi.

--- src/screens/MyExpensesScreen.js
+++ src/screens/MyExpensesScreen.js
@@ -1,92 +1,39 @@
-import React, {useState} from 'react';
-import {FlatList, SafeAreaView, Text, View, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
+import PieChart from 'react-native-pie-chart';
 
 let transactionData = [];
 const MyExpenses = ({route}) => {
   if (route.params === undefined) {
     return null;
   } else {
+    const widthAndHeight = 350;
+    const series = [123, 321];
+    const sliceColor = ['#ff6347', '#008000'];
+    console.log(route.params);
     const {transactions} = route.params;
-    console.log(route);
-
     transactionData.push(transactions);
-    console.log(transactionData);
-    // let mapData = transactionData.map(e => e);
-    // console.log(mapData);
-    const renderTransactions = ({item}) => (
-      <TransactionCard transaction={item} />
-    );
-    const renderKey = item => item.id.toString();
-    const renderSeperator = () => <View style={styles.seperator} />;
-
-    const iconFunc = ({transaction, color}) => {
-      let iconName;
-
-      if (transaction.value === 'Kira') {
-        iconName = 'home-circle';
-      }
-      return <Icon name={iconName} size={20} color={color} />;
-    };
-
-    const TransactionCard = ({transaction}) => {
-      return (
-        <View style={styles.cardContainer} key={transaction}>
-          <View style={styles.innerContainer}>
-            <Text style={styles.title}>{transaction.value}</Text>
-            <Text
-              style={{
-                fontSize: 14,
-                fontWeight: '500',
-                color: transaction.isIncome ? 'green' : 'indianred',
-                marginLeft: 5,
-                marginBottom: 10,
-              }}>
-              {transaction.price} ₺
-            </Text>
-          </View>
-          <View style={styles.iconContainer}>
-            {transaction.value === 'Kira' && (
-              <Icon name={'home'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Market' && (
-              <Icon name={'cart-variant'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Fatura' && (
-              <Icon name={'file'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Eğlence' && (
-              <Icon name={'glass-cocktail'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Giyim' && (
-              <Icon name={'tshirt-crew'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Ulaşım' && (
-              <Icon name={'bus'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Diğer' && (
-              <Icon name={'language-javascript'} size={30} color="indianred" />
-            )}
-            {transaction.value === 'Maaş' && (
-              <Icon name={'cash-multiple'} size={30} color="green" />
-            )}
-            {transaction.value === 'Ek Gelir' && (
-              <Icon name={'cash'} size={30} color="green" />
-            )}
-          </View>
-        </View>
-      );
-    };
 
     return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={transactionData}
-          renderItem={renderTransactions}
-          keyExtractor={renderKey}
-          ItemSeparatorComponent={renderSeperator}
-        />
-        {/* <TransactionCard /> */}
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={styles.totalContainer}>
+            <Text style={styles.incomeTitle}>Toplam Gelir: ₺</Text>
+            <Text style={styles.expenseTitle}>Toplam Gider: ₺</Text>
+            <Text />
+          </View>
+        </View>
+        <View style={styles.piecontainer}>
+          <PieChart
+            widthAndHeight={widthAndHeight}
+            series={series}
+            sliceColor={sliceColor}
+            doughnut={true}
+            coverRadius={0.45}
+            coverFill={'#FFF'}
+            style={styles.piechart}
+          />
+        </View>
       </SafeAreaView>
     );
   }
@@ -94,7 +41,6 @@ const MyExpenses = ({route}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     margin: 5,
     borderWidth: 0.2,
     borderColor: 'lightgrey',
@@ -102,26 +48,14 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: 'lightgray',
   },
-  cardContainer: {
-    flexDirection: 'row',
-    paddingLeft: 5,
-    paddingRight: 5,
+  piecontainer: {
+    margin: 5,
+    borderWidth: 0.2,
+    borderColor: 'lightgrey',
+    elevation: 1,
     borderRadius: 5,
-    backgroundColor: 'gainsboro',
-  },
-  innerContainer: {
-    flex: 1,
-    justifyContent: 'space-evenly',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: 'black',
-    marginLeft: 5,
-    marginTop: 5,
-  },
-  priceContainer: {
-    justifyContent: 'flex-end',
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
   },
   seperator: {
     borderWidth: 0.2,
@@ -129,8 +63,25 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginLeft: 10,
   },
-  iconContainer: {
-    margin: 10,
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  incomeTitle: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: 'green',
+  },
+  expenseTitle: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: 'tomato',
+  },
+  piechart: {
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
